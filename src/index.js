70c7a51642193ab5a1fcbfed0996d5a3188a9c36
Дмитрий Гавриш <demetriusgarson@gmail.com>
2026-09-21 14:20:39 +0300

    node:path, node:fs/promise practice

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-const message = "Hello World!";
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const message = 'Hello World!';
 console.log(message);
+
+const pathToWorkDir = path.join(process.cwd());
+console.log(pathToWorkDir);
+console.log(path.parse(pathToWorkDir));
+
+const text = await fs.readFile(`${pathToWorkDir}/text.txt`, 'utf-8');
+console.log(text);
+
+fs.writeFile('text2.txt', 'test Write File');
+
+fs.appendFile('text3.txt', '\ntest append');
