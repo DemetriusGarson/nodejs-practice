8527a02433a2ba76f48f18f488003911f89c20b6
Дмитрий Гавриш <demetriusgarson@gmail.com>
2026-09-23 15:50:56 +0300

    add all validation schemas

--- src/server.js
+++ src/server.js
@@ -8,15 +8,10 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
 import studentsRoutes from './routes/studentsRoutes.js';
 import { timeLogger } from './middleware/timeLogger.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
-
-const users = [
-  { id: 1, name: 'Alice' },
-  { id: 2, name: 'Alice2' },
-  { id: 3, name: 'Alice3' },
-];
 
 // Логування
 app.use(logger);
@@ -30,46 +25,21 @@ app.use(cors());
 // Приклад логування часу
 app.use(timeLogger);
 
-// підключаємо групу маршрутів студента
-app.use(studentsRoutes);
-
 // Перший маршрут
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello world' });
 });
 
-//Второй маршрут
-app.get('/health', (req, res) => {
-  res.status(200).json({
-    message: 'Health page',
-    status: 'Health OK',
-  });
-});
+// підключаємо групу маршрутів студента
+app.use(studentsRoutes);
 
-//Маршрут по всем юзерам
-app.get('/users', (req, res) => {
-  res.status(200).json(users);
-});
-
-//Динамичиский маршрут юзера по id
-app.get('/users/:Id', (req, res) => {
-  const userId = Number(req.params.Id);
-
-  // const userId = req.params.Id;
-  res.status(200).json(users.find((user) => user.id === userId));
-});
-
-// Маршрут для тестовой ошибки
-app.get('/test-error', (req, res) => {
-  throw new Error('Something went wrong');
-});
-
-//! Модуль 2 /////////////////////////////////////////////
-
-// Пример обработки не существующих маршрутов, добавил переменную path для отображения машрута
+// обробка 404
 app.use(notFoundHandler);
 
-// мидлвер для перехоплення помилок
+// обробка помилок від celebrate (валідація)
+app.use(errors());
+
+// глобальна обробка інших помилок
 app.use(errorHandler);
 
 // підключення до MongoDB
--- src/validations/studentsValidation.js
+++ src/validations/studentsValidation.js
@@ -1,4 +1,17 @@
 import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+
+// Кастомний валідатор для ObjectId
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+// Схема для перевірки параметра studentId
+export const studentIdParamSchema = {
+  [Segments.PARAMS]: Joi.object({
+    studentId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
 
 export const createStudentSchema = {
   [Segments.BODY]: Joi.object({
@@ -28,4 +41,17 @@ export const createStudentSchema = {
       'boolean.base': 'onDuty must be a boolean value',
     }),
   }),
+};
+
+export const updateStudentSchema = {
+  [Segments.PARAMS]: Joi.object({
+    studentId: Joi.string().custom(objectIdValidator).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().min(3).max(30),
+    age: Joi.number().integer().min(12).max(65),
+    gender: Joi.string().valid('male', 'female', 'other'),
+    avgMark: Joi.number().min(2).max(12),
+    onDuty: Joi.boolean(),
+  }).min(1), // важливо: не дозволяємо порожнє тіло
 };
